c53845a5a3748046b176168f715615641c711743
Victoria Kwamboka <victoriamonari9815@gmail.com>
2021-08-30 16:10:17 +0300

    Changes on the simple calculator

--- Ex_1/python-numeric-operations/challenge_2.py
+++ Ex_1/python-numeric-operations/challenge_2.py
@@ -1,16 +1,16 @@
 print('Simple Calculator!')
 
-first_num= input('First number?')
+first_num= input('First number? ')
 if first_num.isnumeric() ==False:
     print('Please input a number')
     exit()
 
 
-operation= input('Operation?')
+operation= input('Operation? ')
 
-second_num= input('Second number?')
+second_num= input('Second number? ')
 if first_num.isnumeric() == False:
-    print('Please input a number')
+    print('Please input a number.')
     exit()
 
 first_num=int(first_num)
@@ -35,7 +35,7 @@ elif operation=='**':
 elif operation=='%':
     result= first_num%second_num
     label='modulus'
-print(label +' of '+ str(first_num)+' '+ operation +' ' + str(second_num)+ 'equals' + str(result))
+print(label +' of '+ str(first_num)+' '+ operation +' ' + str(second_num)+ ' equals ' + str(result))
 
 
 
