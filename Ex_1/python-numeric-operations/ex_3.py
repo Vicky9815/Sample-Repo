726960900092695da3e1ab86fa336d1528f5f421
Victoria Kwamboka <victoriamonari9815@gmail.com>
2021-08-30 12:39:00 +0300

    Mathematical Operations

---
+++ Ex_1/python-numeric-operations/ex_3.py
@@ -0,0 +1,21 @@
+first_num= 27
+second_num= 12
+
+sum= first_num+ second_num
+diff= first_num- second_num
+product= first_num* second_num
+quotient= first_num/second_num
+modulus= first_num % second_num
+exponent= first_num** second_num
+
+# if first_num.isnumeric() == False or second_num.isnumeric()==False:
+#     print('Please enter valid numbers')
+#     exit()
+# else:
+
+print('Sum: ',sum)
+print('Difference: ',diff)
+print('Product: ',product)
+print('Quotient: ',quotient)
+print('Modilus: ',modulus)
+print('Exponent: ',exponent)
